3bdd03c771e1d70bd4a908243125766f6c2aaf21
Ferwski <91921965+Ferwski@users.noreply.github.com>
2022-09-17 01:37:25 -0300

    1 file added

---
+++ 0.2.2.js
@@ -0,0 +1,15 @@
+const user = [
+  {
+    nome: 'Roberto',
+    idade: '36',
+    email: 'robertinho@gmail.com',
+    id_name: 'Beto_36',
+    cidade: 'Porto Alegre',
+    comentario1: [
+      {
+        nome: 'Jorge',
+        comentario: 'Ótimo profissional',
+      },
+    ],
+  },
+];
