6dafb7b4b5c6f2d5fab24880a1cc0997be7ea3bf
Ferwski <91921965+Ferwski@users.noreply.github.com>
2022-09-16 23:59:35 -0300

    Initial commit

---
+++ 0.2.1.js
@@ -0,0 +1,20 @@
+const filmes = [
+  {
+    titulo: 'Harry Potter',
+    ano: 1998,
+    atorPrincipal: 'Daniel Radcliffe',
+    generos: ['acao'],
+  },
+  {
+    titulo: 'Velozes e Furiosos Desafio em Tóquio',
+    ano: 2006,
+    atorPrincipal: 'Lucas Black',
+    generos: ['acao', 'corrida', 'carros'],
+  },
+  {
+    titulo: 'Senhor dos anéis',
+    ano: 2001,
+    atorPrincipal: 'Elijah Wood',
+    generos: ['acao'],
+  },
+];
---
+++ README.md
@@ -0,0 +1,3 @@
+# node-unuutp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-unuutp)
