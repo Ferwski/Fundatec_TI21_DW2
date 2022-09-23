999d8ef74b4cc0cbb1374b12e3bc84fe3012c027
Ferwski <91921965+Ferwski@users.noreply.github.com>
2022-09-23 19:53:10 -0300

    1 file modified

--- 0.2.2.js
+++ 0.2.2.js
@@ -1,7 +1,7 @@
 const user = [
   {
     nome: 'Roberto',
-    idade: '36',
+    anoNascimento '1997',
     email: 'robertinho@gmail.com',
     id_name: 'Beto_36',
     cidade: 'Porto Alegre',
@@ -9,6 +9,18 @@ const user = [
       {
         nome: 'Jorge',
         comentario: 'Ótimo profissional',
+      },
+    ],
+    comentario2: [
+      {
+        nome: 'Gabriel',
+        comentario: 'Top',
+      },
+    ],
+    comentario3: [
+      {
+        nome: 'Roberta',
+        comentario: 'Competente',
       },
     ],
   },
